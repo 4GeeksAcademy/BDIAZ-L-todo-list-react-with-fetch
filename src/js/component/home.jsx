9f46d9d0d5a370d71b4fe81e871eb05005c2158e
BDiaz-L <79861209+BDiaz-L@users.noreply.github.com>
2024-11-05 17:33:56 +0000

    Agregamos estructura de la practica anterior

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,45 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState, useEffect}from "react";
 
 //create your first component
 const Home = () => {
+
+	const [newTask, setNewTask] = useState("");
+	const [tasks, setTasks] = useState(["No tasks here, add tasks"]);
+
+	
+	const TaskChanger = (event) => {
+		if (event.target.value !== "") {setNewTask(event.target.value)}
+	}
+	const addTask = (event) => {
+		if (event.target.value !== "") {
+			if (event.key == "Enter") {
+				if (tasks[0] === "No tasks here, add tasks") {
+					tasks[0] = newTask;
+				} else {
+					tasks.push(newTask);
+				}
+				setNewTask("")
+			}	
+		}
+	}
+
+	const deteleTask = (event, item) => {
+		setTasks(tasks.filter((i) => i !== item));
+	}
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container mt-5">
+			<h1 className="todo-header text-center">Todos</h1>
+			<input className="form-control" type="text" onChange={TaskChanger} onKeyDown={addTask} value={newTask} placeholder="Add to do here"/>
+			<ul className="list-group">
+				{tasks.map((item, index) => {
+					return <li key={index} className="list-group-item"><span onClick={(e)=>{deteleTask(e,item)}}><i className="fa fa-trash" item={item}></i></span>{item}</li>
+				})}
+				<li className="list-group-item paper"><small className="">{tasks.length} item left</small></li>
+	  		</ul>
 		</div>
+
 	);
 };
 
